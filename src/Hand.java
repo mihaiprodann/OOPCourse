a1b9391fbca2ae813008754e3dfd5f113a6a0ab4
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2023-10-23 12:27:39 +0300

    update hand class, create sortable interface

---
+++ src/Hand.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Hand implements Sortable{
+    private Card[] hand;
+    private int handSize;
+
+    public Hand(int handSize){
+        this.handSize = handSize;
+        hand = new Card[handSize];
+    }
+
+    public int addCard(Card card){
+        for(int i = 0; i < handSize; i++){
+            if(hand[i] == null){
+                hand[i] = card;
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public int deleteCard(Card.Rank rank, Card.Suit suit){
+        for(int i = 0; i < handSize; i++){
+            if(hand[i].getRank() == rank && hand[i].getSuit() == suit){
+                hand[i] = null;
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public void sort(){
+        Card temp;
+        for(int i = 0; i < handSize; i++){
+            for(int j = 0; j < handSize; j++){
+                if(hand[i] != null && hand[j] != null){
+                    if(hand[i].getRank().ordinal() < hand[j].getRank().ordinal()){
+                        temp = hand[i];
+                        hand[i] = hand[j];
+                        hand[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
