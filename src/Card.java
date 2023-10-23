2c6c9c682c35f7193e8d0143378fd8511ee6bdc8
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2023-10-23 12:15:58 +0300

    update card class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,25 @@
 public class Card {
+    public Rank rank;
+    public Suit suit;
+    public Card(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+    enum Rank{
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, KNIGHT, QUEEN, KING
+    }
+    enum Suit{
+        CLUBS, DIAMONDS, HEARTS, SPADES
+    }
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
